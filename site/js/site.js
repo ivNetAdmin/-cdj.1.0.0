d6bdf9bd67b52724849e81dc069e43cf4e3e9a99
ivNetAdmin <bp@ivnet.co.uk>
2019-03-15 01:24:26 +0000

    gallery filter

--- site/js/site.js
+++ site/js/site.js
@@ -21,6 +21,28 @@ $(function() {
      },1000);
    });
 
+   $(".filter-button").click(function(){
+    var value = $(this).attr('data-filter');
+    
+    if(value == "all")
+    {
+        //$('.filter').removeClass('hidden');
+        $('.filter').show('1000');
+    }
+    else
+    {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+        $(".filter").not('.'+value).hide('3000');
+        $('.filter').filter('.'+value).show('3000');
+        
+    }
+});
+
+if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
 
    var $imageSrc;  
 $('.gallery img').click(function() {
